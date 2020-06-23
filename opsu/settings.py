79c099b6a6395f2b19c9607a96e3dc788ab9862f
gvizquel <gvizquel@gmail.com>
2020-06-23 13:01:00 -0400

    DEBUG fix

--- opsu/settings.py
+++ opsu/settings.py
@@ -19,6 +19,8 @@ SECRET_KEY = ENVIROMENT("SECRET_KEY")
 
 DEBUG = ENVIROMENT("DEBUG")
 
+print(DEBUG)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(BASE_DIR)
