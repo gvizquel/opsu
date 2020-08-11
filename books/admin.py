401d52ad0309dacf01fb56952d7e1f29fd35c59a
gvizquel <gvizquel@gmail.com>
2020-08-11 12:40:03 -0400

    Publicaciones

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,32 @@
 # Django Libraries
 from django.contrib import admin
 
+# Thirdparty Libraries
+from books.models import Autor, Categoria, Editor, Editorial, Publicacion
+
 # Register your models here.
+
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Editor)
+class EditorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Editorial)
+class EditorialAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Publicacion)
+class PublicacionAdmin(admin.ModelAdmin):
+    pass
